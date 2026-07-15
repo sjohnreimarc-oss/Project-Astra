9d97a405d85d7421f2222a95fa40ce3bf1a177de
John Reimarc <reimarcsohitado@gmail.com>
2026-07-15 16:03:08 +0800

    Added a new .py file for constants.

---
+++ constants.py
@@ -0,0 +1,58 @@
+# Gravitational Parameter (km^3/s^2)
+mu = {
+    "Earth": 398600,
+    "Moon": 4902.8,
+    "Sun": 132712440018,
+    "Mercury": 22032,
+    "Venus": 324859,
+    "Mars": 42828,
+    "Jupiter": 126686534,
+    "Saturn": 37931187,
+    "Uranus": 5793939,
+    "Neptune": 6836529
+}
+
+
+# Mean Radius (km)
+mean_radius = {
+    "Earth": 6371.8,
+    "Moon": 1737.4,
+    "Sun": 695700,
+    "Mercury": 2439.7,
+    "Venus": 6051.8,
+    "Mars": 3389.5,
+    "Jupiter": 69911,
+    "Saturn": 58232,
+    "Uranus": 25362,
+    "Neptune": 24622
+}
+
+
+# Surface Gravity (m/s^2)
+surface_gravity = {
+    "Earth": 9.81,
+    "Moon": 1.62,
+    "Sun": 274.0,
+    "Mercury": 3.70,
+    "Venus": 8.87,
+    "Mars": 3.72,
+    "Jupiter": 24.79,
+    "Saturn": 10.44,
+    "Uranus": 8.69,
+    "Neptune": 11.15
+}
+
+
+# Average Orbital Radius Around the Sun (km)
+# Moon is around the Earth.
+orbital_radius = {
+    "Mercury": 57909227,
+    "Venus": 108209475,
+    "Earth": 149597870,
+    "Moon": 384400,
+    "Mars": 227939200,
+    "Jupiter": 778340821,
+    "Saturn": 1433449370,
+    "Uranus": 2870658186,
+    "Neptune": 4498396441
+}
